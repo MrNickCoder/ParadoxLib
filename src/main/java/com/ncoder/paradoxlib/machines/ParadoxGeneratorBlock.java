dc6843fb40a09e20fb6218b6e21ed86178f55cb7
Mr_NickCoder <rexgorre@gmail.com>
2022-03-24 04:02:35 +0800

    Update ParadoxGeneratorBlock.java

--- src/main/java/com/ncoder/paradoxlib/machines/ParadoxGeneratorBlock.java
+++ src/main/java/com/ncoder/paradoxlib/machines/ParadoxGeneratorBlock.java
@@ -5,26 +5,47 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
 import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
+
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
 
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
 
-public abstract class ParadoxGeneratorBlock extends ParadoxTickingBlock implements EnergyNetProvider {
+import java.util.Objects;
 
-    protected int energyPerTick = -1;
-    protected int energyCapacity = -1;
+public abstract class ParadoxGeneratorBlock extends ParadoxInventoryBlock implements EnergyNetProvider {
 
     public ParadoxGeneratorBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
     }
 
     @Override
-    protected void tick(Block b, BlockMenu menu) {
+    public int getGeneratedOutput(Location l, Config config) {
+        BlockMenu menu = BlockStorage.getInventory(l);
 
+        int gen = generate(l.getWorld(), l.getBlock(), menu);
+
+        if (menu.hasViewer()) {
+            if (gen == 0) {
+                menu.replaceExistingItem(getStatusSlot(), NOT_GENERATING);
+            } else {
+                menu.replaceExistingItem(getStatusSlot(), GENERATING);
+            }
+        }
+
+        return gen;
     }
+
+    public abstract int generate(World world, Block block, BlockMenu menu);
+
+    protected abstract int getStatusSlot();
 
     @Nonnull
     @Override
