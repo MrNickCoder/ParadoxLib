3fd371c4de38714578a5a4ab242f0e410602c693
Mr_NickCoder <rexgorre@gmail.com>
2022-03-24 03:37:27 +0800

    Update BlockBreakListener.java

--- src/main/java/com/ncoder/paradoxlib/core/listeners/BlockBreakListener.java
+++ src/main/java/com/ncoder/paradoxlib/core/listeners/BlockBreakListener.java
@@ -66,12 +66,34 @@ public class BlockBreakListener implements Listener {
     }
 
     public boolean validateOre(final Material material) {
-        if (material.name().endsWith("_ORE") || material.name().equals("ANCIENT_DEBRIS")) return true;
+        String name = material.name();
+        if (name.endsWith("_ORE")) return true;
+        if (name.equals("ANCIENT_DEBRIS")) return true;
         return false;
     }
 
     public boolean validateWood(final Material material) {
-        if (material.name().endsWith("_LOG") || material.name().endsWith("_WOOD") || material.name().endsWith("_STEM") || material.name().endsWith("_YPHAE")) return true;
+        String name = material.name();
+        if (name.endsWith("_LOG")) return true;
+        if (name.endsWith("_WOOD")) return true;
+        if (name.endsWith("_STEM")) return true;
+        if (name.endsWith("_HYPHAE")) return true;
+        return false;
+    }
+
+    public boolean validatePlant(final Material material) {
+        String name = material.name();
+        if (name.equals("GRASS") || name.equals("TALL_GRASS") || name.equals("FERN") || name.equals("LARGE_FERN") || name.equals("SEAGRASS") || name.equals("TALL_SEAGRASS")) return true;
+        if (name.equals("CARROTS") || name.equals("POTATOES") || name.equals("WHEAT") || name.equals("BEETROOTS")) return true;
+        if (name.equals("DEAD_BUSH") || name.equals("SWEET_BERRY_BUSH")) return true;
+        if (name.equals("PUMPKIN") || name.equals("MELON")) return true;
+        if (name.equals("BAMBOO") || name.equals("SUGAR_CANE")) return true;
+        if (name.equals("CACTUS")) return true;
+        if (name.equals("COCOA")) return true;
+        if (name.equals("DANDELION") || name.equals("POPPY") || name.equals("BLUE_ORCHID") || name.equals("ALLIUM") ||
+                name.equals("AZURE_BLUET") || name.equals("OXEYE_DAISY") || name.equals("CORNFLOWER") || name.equals("LILY_OF_THE_VALLEY") ||
+                name.equals("WITHER_ROSE") || name.equals("SUNFLOWER") || name.equals("LILAC") || name.equals("ROSE_BUSH") ||
+                name.equals("PEONY") || (!name.startsWith("POTTED_") && name.endsWith("_TULIP"))) return true;
         return false;
     }
 
