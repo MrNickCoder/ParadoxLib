770242c65d9926edbc9af0f6157812641f3c2145
Mr_NickCoder <rexgorre@gmail.com>
2022-03-24 02:17:38 +0800

    Update BlockBreakListener.java

--- src/main/java/com/ncoder/paradoxlib/core/listeners/BlockBreakListener.java
+++ src/main/java/com/ncoder/paradoxlib/core/listeners/BlockBreakListener.java
@@ -1,7 +1,12 @@
 package com.ncoder.paradoxlib.core.listeners;
 
+import com.ncoder.paradoxlib.core.ParadoxAPIs;
 import com.ncoder.paradoxlib.events.PlayerBreakOreEvent;
 import com.ncoder.paradoxlib.events.PlayerBreakWoodEvent;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+
+import net.coreprotect.CoreProtectAPI;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -14,17 +19,49 @@ public class BlockBreakListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockBreak(final BlockBreakEvent e) {
         if (e.getPlayer() == null) return;
+        if (BlockStorage.check(e.getBlock()) != null) return;
+
         if (validateOre(e.getBlock().getType())) {
-            PlayerBreakOreEvent playerBreakOreEvent = new PlayerBreakOreEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop());
-            Bukkit.getServer().getPluginManager().callEvent(playerBreakOreEvent);
-            if (playerBreakOreEvent.isCancelled()) e.setCancelled(true);
-            e.setExpToDrop(playerBreakOreEvent.getExpToDrop());
+            if (ParadoxAPIs.getCoreProtectAPI() != null) {
+                CoreProtectAPI api = ParadoxAPIs.getCoreProtectAPI();
+                if (api.blockLookup(e.getBlock(), Integer.MAX_VALUE) == null) {
+                    PlayerBreakOreEvent playerBreakOreEvent = new PlayerBreakOreEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), false);
+                    Bukkit.getServer().getPluginManager().callEvent(playerBreakOreEvent);
+                    if (playerBreakOreEvent.isCancelled()) e.setCancelled(true);
+                    e.setExpToDrop(playerBreakOreEvent.getExpToDrop());
+                } else {
+                    PlayerBreakOreEvent playerBreakOreEvent = new PlayerBreakOreEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), false);
+                    Bukkit.getServer().getPluginManager().callEvent(playerBreakOreEvent);
+                    if (playerBreakOreEvent.isCancelled()) e.setCancelled(true);
+                    e.setExpToDrop(playerBreakOreEvent.getExpToDrop());
+                }
+            } else {
+                PlayerBreakOreEvent playerBreakOreEvent = new PlayerBreakOreEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), true);
+                Bukkit.getServer().getPluginManager().callEvent(playerBreakOreEvent);
+                if (playerBreakOreEvent.isCancelled()) e.setCancelled(true);
+                e.setExpToDrop(playerBreakOreEvent.getExpToDrop());
+            }
         }
         if (validateWood(e.getBlock().getType())) {
-            PlayerBreakWoodEvent playerBreakWoodEvent = new PlayerBreakWoodEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop());
-            Bukkit.getServer().getPluginManager().callEvent(playerBreakWoodEvent);
-            if (playerBreakWoodEvent.isCancelled()) e.setCancelled(true);
-            e.setExpToDrop(playerBreakWoodEvent.getExpToDrop());
+            if (ParadoxAPIs.getCoreProtectAPI() != null) {
+                CoreProtectAPI api = ParadoxAPIs.getCoreProtectAPI();
+                if (api.blockLookup(e.getBlock(), Integer.MAX_VALUE) == null) {
+                    PlayerBreakWoodEvent playerBreakWoodEvent = new PlayerBreakWoodEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), true);
+                    Bukkit.getServer().getPluginManager().callEvent(playerBreakWoodEvent);
+                    if (playerBreakWoodEvent.isCancelled()) e.setCancelled(true);
+                    e.setExpToDrop(playerBreakWoodEvent.getExpToDrop());
+                } else {
+                    PlayerBreakWoodEvent playerBreakWoodEvent = new PlayerBreakWoodEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), false);
+                    Bukkit.getServer().getPluginManager().callEvent(playerBreakWoodEvent);
+                    if (playerBreakWoodEvent.isCancelled()) e.setCancelled(true);
+                    e.setExpToDrop(playerBreakWoodEvent.getExpToDrop());
+                }
+            } else {
+                PlayerBreakWoodEvent playerBreakWoodEvent = new PlayerBreakWoodEvent(e.getPlayer(), e.getPlayer().getItemInUse(), e.getBlock(), e.getExpToDrop(), true);
+                Bukkit.getServer().getPluginManager().callEvent(playerBreakWoodEvent);
+                if (playerBreakWoodEvent.isCancelled()) e.setCancelled(true);
+                e.setExpToDrop(playerBreakWoodEvent.getExpToDrop());
+            }
         }
     }
 
