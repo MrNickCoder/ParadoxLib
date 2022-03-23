d0040bac2570df8e77d11531a93ed896a977ec3b
Mr_NickCoder <rexgorre@gmail.com>
2022-03-23 16:12:01 +0800

    Update ParadoxGeneratorBlock.java

--- src/main/java/com/ncoder/paradoxlib/machines/ParadoxGeneratorBlock.java
+++ src/main/java/com/ncoder/paradoxlib/machines/ParadoxGeneratorBlock.java
@@ -1,4 +1,33 @@
 package com.ncoder.paradoxlib.machines;
 
-public class ParadoxGeneratorBlock {
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetProvider;
+import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+
+public abstract class ParadoxGeneratorBlock extends ParadoxTickingBlock implements EnergyNetProvider {
+
+    protected int energyPerTick = -1;
+    protected int energyCapacity = -1;
+
+    public ParadoxGeneratorBlock(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
+    }
+
+    @Override
+    protected void tick(Block b, BlockMenu menu) {
+
+    }
+
+    @Nonnull
+    @Override
+    public final EnergyNetComponentType getEnergyComponentType() { return EnergyNetComponentType.GENERATOR; }
+
 }
