0adee0ddacdbdb17f497c0a946910c3be0f253ba
Mr_NickCoder <rexgorre@gmail.com>
2022-03-25 04:13:08 +0800

    Create GlowEnchant.java

---
+++ src/main/java/com/ncoder/paradoxlib/enchants/GlowEnchant.java
@@ -0,0 +1,63 @@
+package com.ncoder.paradoxlib.enchants;
+
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.annotation.Nonnull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class GlowEnchant extends Enchantment {
+
+    private final Set<String> ids = new HashSet<>();
+
+    public GlowEnchant(@Nonnull NamespacedKey key, @Nonnull String[] applicableItems) {
+        super(key);
+        ids.addAll(Arrays.asList(applicableItems));
+    }
+
+    @Nonnull
+    @Override
+    @Deprecated
+    public String getName() { return "PARADOXLIB_GLOW"; }
+
+    @Override
+    public int getMaxLevel() { return 1; }
+
+    @Override
+    public int getStartLevel() { return 1; }
+
+    @Override
+    public EnchantmentTarget getItemTarget() { return EnchantmentTarget.ALL; }
+
+    @Override
+    public boolean isTreasure() { return false; }
+
+    @Override
+    public boolean isCursed() { return false; }
+
+    @Override
+    public boolean conflictsWith(Enchantment other) { return false; }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        if (item.hasItemMeta()) {
+            final ItemMeta itemMeta = item.getItemMeta();
+            final Optional<String> id = Slimefun.getItemDataService().getItemData(itemMeta);
+
+            if (id.isPresent()) {
+                return ids.contains(id.get());
+            }
+        }
+        return false;
+    }
+
+}
