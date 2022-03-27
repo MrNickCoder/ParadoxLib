c1f68e488befe5180748bdff77d0301642bc6dd7
Mr_NickCoder <rexgorre@gmail.com>
2022-03-28 02:43:52 +0800

    ConversionUtil Update

--- src/main/java/com/ncoder/paradoxlib/utils/ConversionUtil.java
+++ src/main/java/com/ncoder/paradoxlib/utils/ConversionUtil.java
@@ -11,16 +11,15 @@ public class ConversionUtil {
     public static class LOCATION {
 
         public static String toString(@Nonnull Location location) {
-            return location.getWorld().getName() + ";" + location.getX() + ";" + location.getY() + ";" + location.getZ() + ";" + location.getYaw() + ";" + location.getPitch();
+            return String.valueOf(location.getWorld().getName() + ";" + location.getX() + ";" + location.getY() + ";" + location.getZ() + ";" + location.getYaw() + ";" + location.getPitch()).replace('.', ',');
         }
 
         public static Location toLocation(@Nonnull String string) {
-            String[] split = string.split(";");
+            String[] split = string.replace(',', '.').split(";");
             return new Location(Bukkit.getServer().getWorld(split[0]), Double.valueOf(split[1]), Double.valueOf(split[2]), Double.valueOf(split[3]), Float.valueOf(split[4]), Float.valueOf(split[5]));
         }
 
     }
-
 
     public static class NUMERICAL {
 
